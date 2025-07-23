9bafdcfe066472abd87905cbca1cf56fa5dfbaba
Odebode Oluwatimilehin <odebodezion@gmail.com>
2025-07-23 19:35:17 +0100

    refactor: remove unused files

--- src/exceptions/validation.exception.ts
+++
@@ -1,7 +0,0 @@
-import { BadRequestException } from "@nestjs/common";
-
-export class ValidationException extends BadRequestException {
-    constructor(validationErrors: string[]) {
-        super({statusCode: 400, message: validationErrors, error: "Validation Error"});
-    }
-}
