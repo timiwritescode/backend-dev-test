b6ec5b121129267185a3eb189d3f3ad2b897942e
Odebode Oluwatimilehin <odebodezion@gmail.com>
2025-07-24 19:03:12 +0100

    test: add unit tests for admin user controller

---
+++ src/api/admin/users/tests/admin.user.controller.test.ts
@@ -0,0 +1,148 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { AdminUsersController } from "../admin.user.controller"
+import { AdminUsersService } from "../admin.user.service";
+import { FirebaseAdmin } from "src/config/firebase.config";
+import { AdminModule } from "../../admin.module";
+import { FirebaseAuthGuard } from "src/guards/firebaseAuth.guard";
+import { RolesGuard } from "src/guards/roles.guard";
+import { mockGetAllUsers, mockGetUserById, mockGetUserCompanies, mockGetUserCompanyById } from "./util/mocks";
+import { MultipleUsersDto } from "src/api/user/dto/multipleUsers.dto";
+import { GeneralResponseDto } from "src/dto/generalResponse.dto";
+import { UserDto } from "src/api/user/dto/user.dto";
+import { MultipleUserCompaniesDto } from "src/api/company/dto/multipleCompanies.dto";
+import { CompanyDto } from "src/api/company/dto/company.dto";
+
+describe("AdminUserController", () => {
+    let controller: AdminUsersController;
+    
+    const mockAdminUserService = {
+        getAllUsers: mockGetAllUsers,
+        getUserById: mockGetUserById,
+        getUserCompanies: mockGetUserCompanies,
+        getUserCompanyById: mockGetUserCompanyById
+    }
+
+    beforeEach(async () => {
+             const module: TestingModule = await Test.createTestingModule({
+                 controllers: [AdminUsersController],
+                 providers: [AdminUsersService]
+             }).overrideProvider(AdminUsersService)
+                .useValue(mockAdminUserService)
+                .overrideGuard(FirebaseAuthGuard)
+                .useValue({})
+                .overrideGuard(RolesGuard)
+                .useValue({})
+                .compile();
+    
+             controller = module.get<AdminUsersController>(AdminUsersController);
+         })
+    
+    
+         it('should be defined', () => {
+            expect(controller).toBeDefined();
+         });
+
+         it("should return GeneralResponseDto containing a data body of user objects", async () => {
+            const page = 1;
+            const pageSize = 10;
+
+            const mockResponse: GeneralResponseDto<MultipleUsersDto> = {
+                success: true,
+                description: expect.any(String),
+                data: expect.objectContaining({
+                    users: expect.arrayContaining([
+                        expect.objectContaining({
+                            userId: expect.any(String),
+                            username: expect.any(String),
+                            email: expect.any(String),
+                            role: expect.any(String)
+                        })
+                    ])
+                })
+            }
+
+            expect(await controller.getUsers(page, pageSize)).toEqual(mockResponse);
+            expect(mockAdminUserService.getAllUsers).toHaveBeenCalled();
+            expect(mockAdminUserService.getAllUsers).toHaveBeenCalledWith(page, pageSize)
+         })
+
+
+         it ("should return GeneralResponseDto with a data field containing a user object", async () => {
+            const mockUserId = "mock_user_id"
+            const mockResponse: GeneralResponseDto<UserDto> = {
+                success: true,
+                description: expect.any(String),
+                data: {
+                    email: expect.any(String),
+                    userId: expect.any(String),
+                    username: expect.any(String),
+                    role: expect.any(String)
+                }
+            }
+
+            expect(await controller.getUserById(mockUserId)).toEqual(mockResponse);
+            expect(mockAdminUserService.getUserById).toHaveBeenCalled();
+            expect(mockAdminUserService.getUserById).toHaveBeenCalledWith(mockUserId)
+         })
+
+         it ("should return GeneralResponseDto with a data body of company objects", async () => {
+            const mockUserId = "mock_user_id"
+            const page = 1;
+            const pageSize = 10;
+            const mockResponse: GeneralResponseDto<MultipleUserCompaniesDto> = {
+                success: true,
+                description: expect.any(String),
+                data: expect.objectContaining({
+                    companies: expect.arrayContaining([
+                        expect.objectContaining({
+                            companyName: expect.any(String),
+                            companyId: expect.any(String),
+                            numberOfUsers: expect.any(Number),
+                            numberOfProducts: expect.any(Number),
+                            percentage: expect.any(Number),
+                            user: expect.objectContaining({
+                                email: expect.any(String),
+                                userId: expect.any(String),
+                                username: expect.any(String),
+                                role: expect.any(String)
+                            } as UserDto)
+                        }as CompanyDto)
+                    ])
+                })
+            }
+
+            expect(await controller.getUserCompanies(mockUserId, page, pageSize)).toEqual(mockResponse);
+            expect(mockAdminUserService.getUserCompanies).toHaveBeenCalled()
+            expect(mockAdminUserService.getUserCompanies).toHaveBeenCalledWith(mockUserId, page, pageSize)
+
+         })
+
+
+        it("should return GeneralResponseDto with a data body containing a Company Object", async () => {
+            const mockUserId = "mockUserId";
+            const mockCompanyId = "mockCompanyId";
+
+            const mockResponse: GeneralResponseDto<CompanyDto> = {
+                success: true,
+                description: expect.any(String),
+                data: expect.objectContaining({
+                            companyName: expect.any(String),
+                            companyId: expect.any(String),
+                            numberOfUsers: expect.any(Number),
+                            numberOfProducts: expect.any(Number),
+                            percentage: expect.any(Number),
+                            user: expect.objectContaining({
+                                email: expect.any(String),
+                                userId: expect.any(String),
+                                username: expect.any(String),
+                                role: expect.any(String)
+                            } as UserDto)
+                        }as CompanyDto)
+            };
+
+            expect(await controller.getUserCompanyById(mockUserId, mockCompanyId)).toEqual(mockResponse)
+            expect(mockAdminUserService.getUserCompanyById).toHaveBeenCalled()
+            expect(mockAdminUserService.getUserCompanyById).toHaveBeenCalledWith(mockUserId, mockCompanyId)
+  
+        })
+})
---
+++ src/api/admin/users/tests/util/mocks.ts
@@ -0,0 +1,78 @@
+import { CompanyDto } from "src/api/company/dto/company.dto"
+import { MultipleUserCompaniesDto } from "src/api/company/dto/multipleCompanies.dto"
+import { MultipleUsersDto } from "src/api/user/dto/multipleUsers.dto"
+import { UserDto } from "src/api/user/dto/user.dto"
+import { GeneralResponseDto } from "src/dto/generalResponse.dto"
+
+export const mockGetAllUsers = jest.fn().mockImplementation((page, pageSize): GeneralResponseDto<MultipleUsersDto> => {
+    return {
+        success: true,
+        description: "Successful",
+        data: {
+            users: [{
+                email: "email_1@test.co",
+                username: "user_1",
+                userId: "userID_1",
+                role: "user"
+            }]
+        }
+    }
+})
+
+
+export const mockGetUserById = jest.fn().mockImplementation((userId): GeneralResponseDto<UserDto> => {
+    return {
+        success: true,
+        description: "Successful",
+        data: {
+            email: "email_1@test.co",
+            username: "test_username",
+            userId: "test_userId",
+            role: "user"
+        }
+    }
+})
+
+
+export const mockGetUserCompanies = jest.fn().mockImplementation((userId, page, pageSize): GeneralResponseDto<MultipleUserCompaniesDto> => {
+    return {
+        success: true,
+        description: "Successful",
+        data: {
+            companies: [{
+                companyName: "Mock company name",
+                companyId: "Mock company ID",
+                numberOfProducts: 10,
+                numberOfUsers: 2000,
+                percentage: 10,
+                user: {
+                    email: "mock@email.co",
+                    userId: "mock_user_id",
+                    username: "mock username",
+                    role:"user"
+                }
+            }]
+        }
+    }
+})
+
+
+export const mockGetUserCompanyById = jest.fn().mockImplementation((userId, companyId): GeneralResponseDto<CompanyDto> => {
+    return {
+        success: true,
+        description: "Successful",
+        data: {
+                companyName: "Mock company name",
+                companyId: "Mock company ID",
+                numberOfProducts: 10,
+                numberOfUsers: 2000,
+                percentage: 10,
+                user: {
+                    email: "mock@email.co",
+                    userId: "mock_user_id",
+                    username: "mock username",
+                    role:"user"
+                }
+            }
+    }
+})
