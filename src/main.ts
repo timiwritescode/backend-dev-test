4fc601ae8be3f831e00110e1a2bfb5ef0084c25c
Odebode Oluwatimilehin <odebodezion@gmail.com>
2025-07-24 09:41:39 +0100

    chore: add security middlewares to application

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,22 @@ import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './filters/globalError.filter';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { ValidationError } from 'class-validator';
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.enableCors()
+  
   app.useGlobalPipes(new ValidationPipe({
     exceptionFactory: (validationErrors: ValidationError[] = []) => {
       return new BadRequestException(
         validationErrors.map((error) => Object.values(error.constraints).join('; ')).join('; ')
         )
     }
-  }));;
+  }));
+  app.use(helmet);
+  app.enableCors();
   app.setGlobalPrefix("/api/v1")
   
   await app.listen(process.env.PORT || 3000);
